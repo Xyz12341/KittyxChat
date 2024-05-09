f2b75682eb9a16339b3b56df2d246987909075c2
𝆺꯭𝅥⏤⵿⵿꯭꤫꤫︭ꭙ‌³⛦⃕͜💫🇲σ᭡፝֟ɳ🌙─‌⃛•𝆺꯭𝅥༎𓆪 <160732506+Moonshining1@users.noreply.github.com>
2024-05-09 20:21:59 +0530

    Update ping.py

--- ANNIECHATBOT/modules/ping.py
+++ ANNIECHATBOT/modules/ping.py
@@ -20,15 +20,15 @@ from ANNIECHATBOT.modules.helpers import PNG_BTN
 
 # Random Start Images
 IMG = [
-    "https://telegra.ph/file/61670d3373f2c8bdc2bcb.png",
-    "https://telegra.ph/file/74d333a2eb853d6d340e5.png",
-    "https://telegra.ph/file/e29c7352ac8a29c6d7d1c.png",
-    "https://telegra.ph/file/024c4d788089a549f7c18.png",
-    "https://telegra.ph/file/21a6c46b6b997c9baa507.png",
-    "https://telegra.ph/file/e4205a5896e9cd1354df4.png",
-    "https://telegra.ph/file/ceaee4640a3af5acdb717.png",
-    "https://telegra.ph/file/98f40c919b0598586d697.png",
-    "https://graph.org/file/36af423228372b8899f20.jpg",
+    "https://telegra.ph/file/3eb5af1f3c31bf68073f6.jpg",
+    "https://telegra.ph/file/3eb5af1f3c31bf68073f6.jpg",
+    "https://telegra.ph/file/3eb5af1f3c31bf68073f6.jpg",
+    "https://telegra.ph/file/3eb5af1f3c31bf68073f6.jpg",
+    "https://telegra.ph/file/3eb5af1f3c31bf68073f6.jpg",
+    "https://telegra.ph/file/3eb5af1f3c31bf68073f6.jpg",
+    "https://telegra.ph/file/3eb5af1f3c31bf68073f6.jpg",
+    "https://telegra.ph/file/3eb5af1f3c31bf68073f6.jpg",
+    "https://telegra.ph/file/3eb5af1f3c31bf68073f6.jpg",
   
 ]
 
@@ -49,7 +49,7 @@ STICKER = [
 
 
 
-@app.on_cmd("Annieping")
+@app.on_cmd("Kittyping")
 async def ping(_, message: Message):
     await message.reply_sticker(sticker=random.choice(STICKER))
     start = datetime.now()
@@ -64,7 +64,7 @@ async def ping(_, message: Message):
 
     ms = (datetime.now() - start).microseconds / 1000
     await loda.edit_text(
-        text=f"нey вαву!!\n{app.name} ιѕ alιve 🥀 αnd worĸιng ғιne wιтн a pιng oғ\n➥ `{ms}` ms\n\n<b>мαdє ву [𝙹ᴀʀᴠɪs](https://t.me/{OWNER_USERNAME}) </b>",
+        text=f"нey вαву!!\n{app.name} ιѕ alιve 🥀 αnd worĸιng ғιne wιтн a pιng oғ\n➥ `{ms}` ms\n\n<b>мαdє ву [🇲σ᭡፝֟ɳ🌙](https://t.me/{OWNER_USERNAME}) </b>",
         reply_markup=InlineKeyboardMarkup(PNG_BTN),
     )
     if message.chat.type == ChatType.PRIVATE:
